f7be48ff166ba2664391e493af94ba50c9430106
vicentecarlos <soniamariadias2012@hotmail.com>
2022-08-04 20:19:04 -0300

    Initial commit

---
+++ aula-01.js
@@ -0,0 +1,18 @@
+//Tipos de dados:
+//String = letras, numeros etc, (caracteres)
+//Bool = Booleano = true or false
+//Number = 123456
+
+//Variavel
+let numero = 3
+//console.log('Vitoria' + numero) //Concatenaçao
+//console.log(`Vitoria ${numero}`)
+
+//Estrutura condicional:
+if (numero === 2) { //se
+    console.log("2")
+} else if (numero === 3) { //senao
+    console.log('3')
+} else {
+    console.log('Valores diferentes')
+}
